eafb305bdd9af0a5a77a9d4203d601e8bd8e447f
pelumimalik4321 <pelumimalik27@gmail.com>
2025-09-28 05:03:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function result() {
+    let nightLodge = document.getElementById("nightBooking").value;
+    let hotelRoom = document.getElementById("nightRoom").value;
+    let includeBreakFast = document.getElementById("nightLunch").checked;
+    let baseCost = 10000;
+
+    //additionalc cost to each room
+    if (hotelRoom === "standard" ){
+        baseCost += 5000;
+    }
+    if(hotelRoom === "deluxe"){
+        baseCost += 7500;
+    }
+    if (hotelRoom === "suit" ){
+        baseCost += 10000;
+    }
+   
+
+    if(nightLodge > 5){
+        document.getElementById("calculate").innerText = "apply a discount of 2000 on the total cost";
+        totalCost -= 2000
+    }
+
+    if(includeBreakFast){
+        baseCost += 2000;
+    }
+     let totalCost = baseCost * nightLodge 
+
+     let message = "The total cost for a " + nightLodge + "-night stay in a " + hotelRoom + " is " + totalCost;
+    console.log(message);
+    document.getElementById("calculate").innerText = message;
+
+
+}
